88c77a709933dc3d75061ca0553835e86390ac7a
skunshd <skunshd@gmail.com>
2018-07-26 21:59:06 +0300

    add winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,17 @@
 import React from "react"
 import { observer } from 'mobx-react';
 import Square from './Square';
+import store from "../store";
 
 
 const Board = observer(
     props => {
         return (
             <ul className="board">
-                { Object.keys(props.store).map(id => <Square key={id} id={id} />)}
+                { store.isWinner &&
+                    <h1>Winner is {store.isWinner === 'x' ? 'CROSS!' : 'NOUGHT!'}</h1>
+                }
+                {Object.keys(props.store).map(id => <Square key={id} id={id} />)}
             </ul>
         )
     }
--- src/models/Root.js
+++ src/models/Root.js
@@ -1,5 +1,15 @@
 import {types} from "mobx-state-tree";
 
+const winnerCases = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 
 const Root = types
     .model({
@@ -11,18 +21,32 @@ const Root = types
         5: '',
         6: '',
         7: '',
-        8: '',
+        8: ''
     })
     .actions(self => {
         let isX = false;
 
         function toggle(id) {
             if(self[id]) return;
+            if(self.isWinner) return;
             self[id] = isX ? 'o' : 'x';
             isX = !isX;
         }
         return { toggle };
-    });
+    })
+    .views(self => ({
+        get isWinner() {
+            let result = false;
+            Object.values(winnerCases)
+                .some(winCase => {
+                    const [first, second, third] = winCase;
+                    const isWinner = self[first] === self[second] && self[second] === self[third];
+                    if(isWinner) result = self[third];
+                    return isWinner;
+            });
+            return result;
+        }
+    }));
 
 
 export default Root;
